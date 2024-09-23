f5917090890b4664ab342c291498a8bf41875ff5
EmulationNerds685 <bhaskartiwari270@gmail.com>
2024-09-23 09:07:16 +0530

    version control added!

---
+++ index.js
@@ -0,0 +1,41 @@
+import express from 'express'
+import ejs from 'ejs'
+import bodyParser from 'body-parser'
+import {dirname} from 'path'
+import {fileURLToPath} from 'url'
+import sillyname from "sillyname"
+import generateStupidName from 'sillyname'
+import { generateFromEmail, generateUsername } from "unique-username-generator";
+const app = express()
+const _dirname=dirname(fileURLToPath(import.meta.url))
+app.use(bodyParser.urlencoded({extended:true})) //middelware
+const port = 3000
+
+function current_date(){
+	const today = new Date();
+const day = String(today.getDate()).padStart(2, '0'); // "14"
+const month = String(today.getMonth() + 1).padStart(2, '0'); // "09"
+const year = today.getFullYear(); // "2024"
+
+const formattedDate = `${day}/${month}/${year}`; // "14/09/2024"
+return formattedDate
+
+}
+app.get('/', (req, res) => {
+	var name = generateUsername("-");
+	res.render('index.ejs', {
+		message: "Hello",
+		name: name
+	});
+})
+app.post('/date',(req,res)=>{
+	console.log(current_date())
+res.render('submit.ejs',{date:current_date()})
+})
+app.post('/post', (req, res) => {
+console.log(req.body)
+})
+
+app.listen(port, () => {
+	console.log(`Server is running on port:${port}`)
+})
---
+++ index2.js
@@ -0,0 +1,20 @@
+import express from "express"
+import ejs from 'ejs'
+import bodyParser from 'body-parser'
+
+const app=express()
+const port=4000
+app.use(bodyParser.urlencoded({extended:true})) //using Middleware: First it receives request from server before handlers
+app.get('/',(req,res)=>{
+    res.render('index.ejs')
+})
+app.post('/submit',(req,res)=>{
+    var email=req.body.email // input of email is stored in variable email
+    var password=req.body.password //input of password is stored in variable password
+    res.send('<h1>User Loginned!</h1>')
+    console.log(email)
+    console.log(password)
+})
+app.listen(port,()=>{
+    console.log(`Server is running in port:${port}`)
+})
