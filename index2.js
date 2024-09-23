92eaa79d2238c3ad6db8b83ecb4a064777ccfcdf
EmulationNerds685 <bhaskartiwari270@gmail.com>
2024-09-23 09:15:23 +0530

    Function Added

--- index2.js
+++ index2.js
@@ -5,6 +5,9 @@ import bodyParser from 'body-parser'
 const app=express()
 const port=4000
 app.use(bodyParser.urlencoded({extended:true})) //using Middleware: First it receives request from server before handlers
+function calc(){
+    console.log("This is Calculator")
+}
 app.get('/',(req,res)=>{
     res.render('index.ejs')
 })
